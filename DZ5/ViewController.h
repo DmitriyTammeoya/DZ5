26ddf5fa939cf4902e448f99a11b9b19f000d6dc
DmitryTamm <td123@list.ru>
2020-02-20 14:39:32 -0800

    123

---
+++ DZ5/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DZ5
+//
+//  Created by IOSfish on 08.02.2020.
+//  Copyright © 2020 IOSfish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DZ5Tests/DZ5Tests.m
@@ -0,0 +1,37 @@
+//
+//  DZ5Tests.m
+//  DZ5Tests
+//
+//  Created by IOSfish on 08.02.2020.
+//  Copyright © 2020 IOSfish. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DZ5Tests : XCTestCase
+
+@end
+
+@implementation DZ5Tests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
